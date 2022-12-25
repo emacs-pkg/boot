3578624f795a6e2c2a83ba1603a5d4f1ddf82ba5
JavaCommons Technologies <javacommons@gmail.com>
2022-12-26 04:18:55 +0900

    .

--- boot.el
+++ boot.el
@@ -27,11 +27,31 @@
       (json-read-from-string (pkg-boot-get-text-from-url url))
     (error nil)))
 
-(xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick"))
-(xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick/contributors"))
+(defun pkg-boot-github-api (repo &optional path)
+  (cond
+   ((or (null path) (equal "" path))
+    (pkg-boot-get-json-from-url (format "https://ungh.cc/repos/%s" repo)))
+   (t
+    (pkg-boot-get-json-from-url (format "https://ungh.cc/repos/%s/%s" repo path)))))
+
+(defun pkg-boot-github-file (repo branch path)
+  (pkg-boot-get-text-from-url (format "https://github.com/%s/raw/%s/%s" repo branch path)))
+
+;;(xprint :raw (pkg-boot-get-text-from-url "https://github.com/emacs-pkg/c-quick/raw/v1.4.4/c-quick.el"))
+(xdump (pkg-boot-github-file "emacs-pkg/c-quick" "v1.4.4" "c-quick.el"))
+
+;;(xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick"))
+(xdump (pkg-boot-github-api "emacs-pkg/c-quick" nil))
+
+;;(xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick/contributors"))
+(xdump (pkg-boot-github-api "emacs-pkg/c-quick" "contributors"))
+
 (xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick/files/main"))
+(xdump (pkg-boot-github-api "emacs-pkg/c-quick" "files/main"))
+
 (xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick/releases/latest"))
-(xprint :raw (pkg-boot-get-text-from-url "https://github.com/emacs-pkg/c-quick/raw/v1.4.4/c-quick.el"))
+(xdump (pkg-boot-github-api "emacs-pkg/c-quick" "releases/latest"))
+
 (xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick/files/v1.4.4"))
 
 (defun pkg-boot-write-text-to-file (path text)
