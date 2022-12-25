ea24cf27d920be7563c85abba92cbc4f84a562e8
JavaCommons Technologies <javacommons@gmail.com>
2022-12-26 03:46:37 +0900

    .

--- boot.el
+++ boot.el
@@ -28,3 +28,20 @@
 (xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick/files/main"))
 (xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick/releases/latest"))
 (xprint :raw (pkg-boot-get-text-from-url "https://github.com/emacs-pkg/c-quick/raw/v1.4.4/c-quick.el"))
+(xdump (pkg-boot-get-json-from-url "https://ungh.cc/repos/emacs-pkg/c-quick/files/v1.4.4"))
+
+(defun pkg-boot-write-text-to-file (path text)
+  (unless (stringp text) (setq text (format "%S" text)))
+  (prog1 text
+    (with-temp-buffer
+      (insert text)
+      ;;(make-directory (file-name-directory path) 'parents)
+      (make-empty-file path 'parents)
+      (write-region (point-min) (point-max) path nil 'quiet))))
+
+(let* ((default-directory "~/")
+       (full-path (file-truename "aaa/bbb/ccc.txt")))
+  (xdump full-path)
+  (pkg-boot-write-text-to-file full-path "hello world!ハロー©")
+  (pkg-boot-write-text-to-file "aaa/bbb/ddd.txt" '(msg . "hello world!ハロー©"))
+  )
